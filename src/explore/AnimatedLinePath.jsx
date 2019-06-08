4f8369f68bfc6ee322becd3214c2f3c7a58f50dc
Gordon Johnson <gordonjohnson@outlook.com>
2019-06-08 01:46:39 -0700

    Improve animation performance

--- src/explore/AnimatedLinePath.jsx
+++ src/explore/AnimatedLinePath.jsx
@@ -1,19 +1,29 @@
 import React, { memo } from "react";
 
 import { LinePath } from "@vx/shape";
-import { Spring, config } from "react-spring/renderprops";
+import { Spring, animated } from "react-spring/renderprops";
 
 const AnimatedLinePath = props => {
+  const { x, y, curve, data, fill = "transparent", ...restProps } = props;
+
   return (
-    <Spring from={{ offset: 100 }} to={{ offset: 0 }}>
+    <Spring native from={{ offset: 100 }} to={{ offset: 0 }}>
       {({ offset }) => {
         return (
-          <LinePath
-            pathLength={100}
-            strokeDasharray={100}
-            strokeDashoffset={offset}
-            {...props}
-          />
+          <LinePath x={x} y={y} curve={curve}>
+            {({ path }) => {
+              return (
+                <animated.path
+                  pathLength={100}
+                  strokeDasharray={100}
+                  strokeDashoffset={offset}
+                  d={path(data)}
+                  fill={fill}
+                  {...restProps}
+                />
+              );
+            }}
+          </LinePath>
         );
       }}
     </Spring>
