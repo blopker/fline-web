6d4a38429000e05b33f360d83f3b5161d79a82a9
Gordon Johnson <gordonjohnson@outlook.com>
2019-06-08 19:12:34 -0700

    Safari doesn't support the SVG pathLength property. We get to animate things the hard way!

--- src/explore/AnimatedLinePath.jsx
+++ src/explore/AnimatedLinePath.jsx
@@ -1,7 +1,7 @@
-import React, { memo } from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import { LinePath } from "@vx/shape";
-import { Spring, animated } from "react-spring/renderprops.cjs";
+import { useSpring, animated } from "react-spring";
 
 /**
  * @vx/LinePath that is animated to look like it's drawing itself
@@ -10,34 +10,57 @@ import { Spring, animated } from "react-spring/renderprops.cjs";
  *
  * Set the LinePath to be a dashed line
  * Set the dash size so large that it completely covers the length of the line
- * Animate the offset position of the dash
+ * Animate the offset position of the dash from 100% to 0% of the line length
  *
  */
 
 const AnimatedLinePath = props => {
-  const { x, y, curve, data, fill = "transparent", ...restProps } = props;
+  const {
+    x,
+    y,
+    curve,
+    data,
+    fill = "transparent",
+    stroke,
+    ...restProps
+  } = props;
+
+  // Use spring in imperative mode by passing a function instead of an object
+  // This allows tighter control over when the animation starts
+  const [animationValues, updateAnimation] = useSpring(() => ({}));
+
+  // In order to animate the line path, we need to know how long it is
+  const [lineLength, setLineLength] = useState(null);
+  const pathRef = useCallback(
+    node => {
+      if (node !== null) {
+        const length = node.getTotalLength();
+        setLineLength(length);
+        updateAnimation({
+          from: { dashOffset: length },
+          to: { dashOffset: 0 }
+        });
+      }
+    },
+    [updateAnimation]
+  );
 
   return (
-    <Spring native from={{ offset: 100 }} to={{ offset: 0 }}>
-      {({ offset }) => {
+    <LinePath x={x} y={y} curve={curve}>
+      {({ path }) => {
         return (
-          <LinePath x={x} y={y} curve={curve}>
-            {({ path }) => {
-              return (
-                <animated.path
-                  pathLength={100}
-                  strokeDasharray={100}
-                  strokeDashoffset={offset}
-                  d={path(data)}
-                  fill={fill}
-                  {...restProps}
-                />
-              );
-            }}
-          </LinePath>
+          <animated.path
+            ref={pathRef}
+            stroke={lineLength ? stroke : "none"}
+            strokeDasharray={lineLength}
+            strokeDashoffset={animationValues.dashOffset}
+            d={path(data)}
+            fill={fill}
+            {...restProps}
+          />
         );
       }}
-    </Spring>
+    </LinePath>
   );
 };
 
@@ -49,4 +72,4 @@ AnimatedLinePath.propTypes = {
   y: PropTypes.oneOfType([PropTypes.func, PropTypes.number])
 };
 
-export default memo(AnimatedLinePath);
+export default AnimatedLinePath;
