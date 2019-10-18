b8b00aa853637ae4b0e5058ef0c2949cd9c5d19f
João Azevedo <joao.azevedo@toptal.com>
2019-10-18 11:41:07 +0100

    add header to graph selector and change to list

--- src/components/compare/GraphSelector.jsx
+++ src/components/compare/GraphSelector.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import ButtonBase from "@material-ui/core/ButtonBase";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import EntryGraph from "../../log/EntryGraph";
 import GraphSelectorDialog from "./GraphSelectorDialog"; 
 import { useDatabase } from "../../databaseContext";
+
+const timeFormatter = new Intl.DateTimeFormat("default", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+});
 
 const useStyles = makeStyles(theme => ({
   cardButton: {
@@ -14,9 +21,20 @@ const useStyles = makeStyles(theme => ({
     height: 180
   },
   grahpSelectorPlaceholder: {
+    height: 224,
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  header: {
+    display: "flex"
+  },
+  headerTime: {
+    flex: "0 0 auto",
+    textAlign: "left",
+    padding: 0,
+    paddingRight: theme.spacing(1),
+    minWidth: 55
   }
 }));
 
@@ -54,16 +72,28 @@ export default function GraphSelector(props) {
   let content;
   if (selectedEntry && bloodGlucoseLevels)
     content = (
-      <EntryGraph
-        className={classes.grahpSelector}
-        bloodGlucoseLevels={bloodGlucoseLevels}
-        entry={selectedEntry}
-      />
+      <>
+        <div className={classes.header}>
+          <ListItemText className={classes.headerTime}>
+            <time dateTime={selectedEntry.date.toISOString()}>
+              {timeFormatter.format(selectedEntry.date)}
+            </time>
+          </ListItemText>
+          <ListItemText>
+            {selectedEntry.description}
+          </ListItemText>
+        </div>
+        <EntryGraph
+          className={classes.grahpSelector}
+          bloodGlucoseLevels={bloodGlucoseLevels}
+          entry={selectedEntry}
+        />
+      </>
     );
   else
     content = (
       <div
-        className={`${classes.grahpSelectorPlaceholder} ${classes.grahpSelector}`}
+        className={classes.grahpSelectorPlaceholder}
       >
         {children}
       </div>
@@ -71,9 +101,9 @@ export default function GraphSelector(props) {
 
   return (
     <>
-      <ButtonBase onClick={handleOpenDialog} className={classes.cardButton}>
+      <ListItem button onClick={handleOpenDialog} className={classes.cardButton}>
         {content}
-      </ButtonBase>
+      </ListItem>
       <GraphSelectorDialog
         isOpen={isOpen}
         onClose={handleCloseDialog}
--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
+import List from "@material-ui/core/List";
 import Button from "@material-ui/core/Button";
 import GraphSelector from "../components/compare/GraphSelector";
 import { useDatabase } from "../databaseContext";
@@ -33,8 +34,10 @@ export default function Compare() {
   return (
     <Container maxWidth="md"  className={classes.container}>
       <Card>
-        <GraphSelector entries={logEntries}>Tap to select first graph</GraphSelector>
-        <GraphSelector entries={logEntries}>Tap to select second graph</GraphSelector>
+        <List dense>
+          <GraphSelector entries={logEntries}>Tap to select first graph</GraphSelector>
+          <GraphSelector entries={logEntries}>Tap to select second graph</GraphSelector>
+        </List>
         <CardActions disableSpacing>
           <Button color="secondary">
             Save to Camera Roll
